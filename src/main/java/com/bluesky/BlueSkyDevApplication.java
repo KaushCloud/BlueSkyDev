cc9a2c5fa6581285d7cc839d4f0f1e277ce4e3a6
kaushpal <kaushcloud@gmail.com>
2018-05-03 23:50:52 +0530

    Add existing files

---
+++ src/main/java/com/bluesky/BlueSkyDevApplication.java
@@ -0,0 +1,12 @@
+package com.bluesky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSkyDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSkyDevApplication.class, args);
+	}
+}
